f5a1588b17ebb9c406c728047b5feacdfbb091b1
joshuabeck82 <49796044+joshuabeck82@users.noreply.github.com>
2023-12-04 12:16:23 -0700

    cloned og project

---
+++ CampspotDemo2/tests/CampgroundSearch.tests.ts
@@ -0,0 +1,39 @@
+import { test, campLocations } from '../fixtures/fixtures'
+
+test.describe('Default State/Behavior Tests', () => {
+    test('Check initial page state', async({campspotPage}) => {
+        await campspotPage.navigateHomeAndValidatePageStartState();
+    });
+
+    test('Check results with default form values', async({campspotPage}) => {
+        await campspotPage.navigateHome();
+        await campspotPage.clickSearch();
+        await campspotPage.validateSearchHashNoResults();
+    })
+});
+
+test.describe('Location Tests', () => {
+    for (const location of campLocations){
+        test(`Testing location '${location}'`, async({campspotPage}) => {
+            await campspotPage.navigateHome();
+            await campspotPage.specifyLocation(location);
+            await campspotPage.clickSearch();
+            await campspotPage.validateSearchHasResults();
+        });
+    }
+})
+
+test.describe('Date Tests', () => { //this should accept inputs and loop through them like the location tests above
+    test('Hard-coded test for next month', async({campspotPage}) => {
+        await campspotPage.navigateHome();
+        await campspotPage.selectDateRange();
+        await campspotPage.clickSearch();
+        await campspotPage.validateSearchHashNoResults();
+    });
+});
+
+//placeholder for future addition
+// test.describe('Guest Tests', () => {
+
+// })
+
